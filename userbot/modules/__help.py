7849ed2b103710ba1488d1d31d36c0466afcd85e
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 10:57:30 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -20,25 +20,25 @@
 
 
 from userbot import BOT_USERNAME, CMD_HELP, bot
-from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, joo_cmd
 
 user = bot.get_me()
 DEFAULTUSER = user.first_name
 CUSTOM_HELP_EMOJI = "✘"
 
 
-@kyy_cmd(pattern="help ?(.*)")
+@joo_cmd(pattern="help ?(.*)")
 async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await edit_or_reply(event, f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @NastyProject**")
+            await edit_or_reply(event, f"**✘ Commands Available In {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @ProjectJoni**")
         else:
-            await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
+            await edit_delete(event, f"**Module** `{args}` **Tidak Tersedia!**")
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@KyyUserbot"
+                BOT_USERNAME, "@JooUserbot"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
