e73457835089b9792b50d1ba5486394b1e2db133
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 12:44:52 +0700

    Update gban_bot.py

--- userbot/modules/gban_bot.py
+++ userbot/modules/gban_bot.py
@@ -5,12 +5,12 @@ Available Commands:
 .ungban REASON"""
 import asyncio
 from userbot import CMD_HELP, owner, CMD_HANDLER as cmd
-from userbot.utils import kyy_cmd
+from userbot.utils import joo_cmd
 from userbot import BOTLOG_CHATID, bot
 # imported from uniborg by @heyworld
 
 
-@kyy_cmd(pattern="gbanb(?: |$)(.*)")
+@joo_cmd(pattern="gbanb(?: |$)(.*)")
 async def _(event):
     if BOTLOG_CHATID is None:
         await event.edit("Set BOTLOG_CHATID in vars otherwise module won't work.")
@@ -29,14 +29,14 @@ async def _(event):
             "/gban [user](tg://user?id={}) {}".format(r_from_id, reason)
         )
     await event.delete()
-    await event.reply("**gbanning...**")
+    await event.reply("**Global Banning...**")
     asyncio.sleep(3.5)
-    await event.edit(f"**User gbanned by {owner}**")
+    await event.edit(f"**User Gbanned by {owner}**")
     asyncio.sleep(5)
     await event.delete()
 
 
-@kyy_cmd(pattern="ungbanb(?: |$)(.*)")
+@joo_cmd(pattern="ungbanb(?: |$)(.*)")
 async def _(event):
     if BOTLOG_CHATID is None:
         await event.edit("Set BOTLOG_CHATID in vars otherwise module won't work.")
@@ -52,15 +52,15 @@ async def _(event):
             "/ungban [user](tg://user?id={}) {}".format(r_from_id, reason)
         )
     await event.delete()
-    await event.reply("**ungbanning...**")
+    await event.reply("**Membatalkan Global Banning...**")
     asyncio.sleep(3.5)
-    await event.edit(f"**User ungbanned by {owner}**")
+    await event.edit(f"**User UngBanned by {owner}**")
     asyncio.sleep(5)
     await event.delete()
 
 CMD_HELP.update({
     "gbanbot": f"`{cmd}gbanb`\
-    \nUsage: globally Ban Bot.\
+    \nUsage: Global Ban Bot.\
     \n\n`{cmd}ungbanb` :\
-    \nUsage: Cancel globally Ban Bot."
+    \nUsage: Membatalkan Global Ban Bot."
 })
