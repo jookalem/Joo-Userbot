886f35dc1089d221e9506f1192f3cc82e38bb212
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 16:47:05 +0700

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -1,9 +1,9 @@
 from time import sleep
 from userbot import CMD_HELP, CMD_HANDLER as cmd
-from userbot.utils import kyy_cmd
+from userbot.utils import joo_cmd
 
 
-@kyy_cmd(pattern='santet(?: |$)(.*)')
+@joo_cmd(pattern='santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("`Anda Telah Mengaktifkan Perintah Santet Online ツ`")
