18455a8f4c9eea37096619e3ff79d331b811f37f
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 17:39:45 +0700

    Update tag_all.py

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -11,7 +11,7 @@ import random
 import re
 
 from userbot import CMD_HELP, bot, CMD_HANDLER as cmd
-from userbot.utils import kyy_cmd
+from userbot.utils import joo_cmd
 
 usernexp = re.compile(r"@(\w{3,32})\[(.+?)\]")
 nameexp = re.compile(r"\[([\w\S]+)\]\(tg://user\?id=(\d+)\)\[(.+?)\]")
@@ -24,7 +24,7 @@ class FlagContainer:
     is_active = False
 
 
-@kyy_cmd(pattern="mention(?: |$)(.*)")
+@joo_cmd(pattern="mention(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -37,7 +37,7 @@ async def _(event):
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
 
-@kyy_cmd(pattern="emojitag(?: |$)(.*)")
+@joo_cmd(pattern="emojitag(?: |$)(.*)")
 async def _(event):
     if event.fwd_from or FlagContainer.is_active:
         return
@@ -83,7 +83,7 @@ async def _(event):
         FlagContainer.is_active = False
 
 
-@kyy_cmd(pattern="all(?: |$)(.*)")
+@joo_cmd(pattern="all(?: |$)(.*)")
 async def _(event):
     if event.fwd_from or FlagContainer.is_active:
         return
