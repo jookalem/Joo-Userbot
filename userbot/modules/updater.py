b0c86390948ae3ee4b98036735142fb2b82837d5
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 18:00:49 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -20,7 +20,7 @@ from userbot import (
     UPSTREAM_REPO_BRANCH
 )
 from userbot.events import register
-from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, joo_cmd
 
 requirements_path = path.join(
     path.dirname(path.dirname(path.dirname(__file__))), 'requirements.txt')
@@ -60,7 +60,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await edit_or_reply(event,
                                 "`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `"
-                                " untuk dapat deploy perubahan terbaru dari ✨ҡʏʏ-υѕєявσт✨.`"
+                                " untuk dapat deploy perubahan terbaru dari ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨.`"
                                 )
             repo.__del__()
             return
@@ -70,7 +70,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
                 break
         if heroku_app is None:
             await edit_delete(event,
-                              f"{txt}\n`Kredensial Heroku tidak valid untuk deploy Kyy-Project dyno.`"
+                              f"{txt}\n`Kredensial Heroku tidak valid untuk deploy Joni-Project dyno.`"
                               )
             return repo.__del__()
         await edit_or_reply(event,
@@ -98,7 +98,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
                               )
         else:
             await edit_delete(event,
-                              "`Kyy-Userbot Berhasil DiUpdate🛃,Restart Tunggu Sebentar`"
+                              "`Joo-Userbot Berhasil DiUpdate🔥,Restart Tunggu Sebentar`"
                               )
 
         if BOTLOG:
@@ -119,9 +119,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    x = await edit_or_reply(event, "**✨ҡʏʏ-υѕєявσт✨** `Berhasil Di Update!`")
+    x = await edit_or_reply(event, "**✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨** `Berhasil Di Update!`")
     await asyncio.sleep(1)
-    await x.edit("**✨ҡʏʏ-υѕєявσт✨** `Di Restart....`")
+    await x.edit("**✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨** `Di Restart....`")
     await asyncio.sleep(1)
     await x.edit("`Mohon Menunggu Beberapa Detik.`")
     await asyncio.sleep(10)
@@ -129,7 +129,7 @@ async def update(event, repo, ups_rem, ac_br):
 
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID, "#BOT \n" "**✨ҡʏʏ-υѕєявσт✨ Telah Di Perbarui.**"
+            BOTLOG_CHATID, "#BOT \n" "**✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨ Telah Di Perbarui.**"
         )
         await asyncio.sleep(100)
         await x.delete()
@@ -140,8 +140,8 @@ async def update(event, repo, ups_rem, ac_br):
     return
 
 
-@kyy_cmd(pattern="update(?: |$)(now|deploy)?")
-@register(incoming=True, from_users=1663258664,
+@joo_cmd(pattern="update(?: |$)(now|deploy)?")
+@register(incoming=True, from_users=860951678,
           pattern=r"^.cupdate(?: |$)(now|deploy)?")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
@@ -195,7 +195,7 @@ async def upstream(event):
 
     if changelog == "" and force_update is False:
         await xx.edit(
-            f"\n✨ҡʏʏ-υѕєявσт✨ Sudah Versi Terbaru || Tunggu Update Terbaru\n"
+            f"\n✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨ Sudah Versi Terbaru || Tunggu Update Terbaru\n"
         )
         await asyncio.sleep(15)
         await xx.delete()
@@ -203,7 +203,7 @@ async def upstream(event):
 
     if conf is None and force_update is False:
         changelog_str = (
-            f"**Pembaruan Untuk ✨ҡʏʏ-υѕєявσт✨ :\n\n⚒️ Pembaruan Data :**\n`{changelog}`"
+            f"**Pembaruan Untuk ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨ :\n\n⚒️ Pembaruan Data :**\n`{changelog}`"
         )
         if len(changelog_str) > 4096:
             await xx.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
@@ -219,7 +219,7 @@ async def upstream(event):
         else:
             await xx.edit(changelog_str)
         return await event.respond(
-            f"**Perintah Untuk Update, Sebagai Berikut.**\n🔰 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`{cmd}update now` (Sementara)\n🔰 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`{cmd}update deploy` (Permanen)\n\n__Untuk Meng Update Fitur Terbaru Dari ✨ҡʏʏ-υѕєявσт✨.__"
+            f"**Perintah Untuk Update, Sebagai Berikut.**\n🔰 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`{cmd}update now` (Sementara)\n🔰 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`{cmd}update deploy` (Permanen)\n\n__Untuk Meng Update Fitur Terbaru Dari ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨.__"
         )
 
     if force_update:
@@ -227,13 +227,13 @@ async def upstream(event):
             "`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`"
         )
     else:
-        await xx.edit("` Proses Update ✨ҡʏʏ-υѕєявσт✨, Loading....1%`")
-        await xx.edit("` Proses Update ✨ҡʏʏ-υѕєявσт✨ Loading....20%`")
-        await xx.edit("` Proses Update ✨ҡʏʏ-υѕєявσт✨, Loading....35%`")
-        await xx.edit("` Proses Update ✨ҡʏʏ-υѕєявσт✨, Loading....77%`")
-        await xx.edit("` Proses Update ✨ҡʏʏ-υѕєявσт✨, Updating...90%`")
+        await xx.edit("` Proses Update ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨, Loading....1%`")
+        await xx.edit("` Proses Update ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨ Loading....20%`")
+        await xx.edit("` Proses Update ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨, Loading....35%`")
+        await xx.edit("` Proses Update ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨, Loading....77%`")
+        await xx.edit("` Proses Update ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨, Updating...90%`")
         await xx.edit(
-            "` Proses Update ✨ҡʏʏ-υѕєявσт✨, Mohon Tunggu Sebentar....100%`"
+            "` Proses Update ✨ᴊᴏᴏ-ᴜѕᴇʀʙᴏᴛ✨, Mohon Tunggu Sebentar....100%`"
         )
 
     if conf == "now":
@@ -250,10 +250,10 @@ async def upstream(event):
 CMD_HELP.update(
     {
         "update": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}update`"
-        "\n• : Untuk Melihat Pembaruan Terbaru Kyy-Userbot."
+        "\n• : Untuk Melihat Pembaruan Terbaru Joo-Userbot."
         f"\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}update now`"
-        "\n• : Memperbarui Kyy-Userbot."
+        "\n• : Memperbarui Joo-Userbot."
         f"\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}update deploy`"
-        "\n• : Memperbarui Kyy-Userbot Dengan Cara Men-Deploy Ulang."
+        "\n• : Memperbarui Joo-Userbot Dengan Cara Men-Deploy Ulang."
     }
 )
