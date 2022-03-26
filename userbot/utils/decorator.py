741301231e301554b5d5d7ffa31c843602171f91
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 22:03:21 +0700

    Update decorator.py

--- userbot/utils/decorator.py
+++ userbot/utils/decorator.py
@@ -21,7 +21,7 @@ from userbot import (
 )
 
 
-def kyy_cmd(
+def joo_cmd(
     pattern: str = None,
     allow_sudo: bool = True,
     disable_edited: bool = False,
@@ -44,25 +44,25 @@ def kyy_cmd(
         args["chats"] = black_list_chats
 
     if pattern is not None:
-        global kyy_reg
+        global joo_reg
         global sudo_reg
         if (
             pattern.startswith(r"\#")
             or not pattern.startswith(r"\#")
             and pattern.startswith(r"^")
         ):
-            kyy_reg = sudo_reg = re.compile(pattern)
+            joo_reg = sudo_reg = re.compile(pattern)
         else:
-            kyy_ = "\\" + CMD_HANDLER
+            joo_ = "\\" + CMD_HANDLER
             sudo_ = "\\" + SUDO_HANDLER
-            kyy_reg = re.compile(kyy_ + pattern)
+            joo_reg = re.compile(joo_ + pattern)
             sudo_reg = re.compile(sudo_ + pattern)
             if command is not None:
-                cmd1 = kyy_ + command
+                cmd1 = joo_ + command
                 cmd2 = sudo_ + command
             else:
                 cmd1 = (
-                    (kyy_ +
+                    (joo_ +
                      pattern).replace(
                         "$",
                         "").replace(
@@ -112,7 +112,7 @@ def kyy_cmd(
     return decorator
 
 
-def kyy_handler(
+def joo_handler(
     **args,
 ):
     def decorator(func):
