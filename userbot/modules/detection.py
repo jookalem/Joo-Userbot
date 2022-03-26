07c8359b1460c0228278d1f737c1348dc8c09c49
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 11:53:10 +0700

    Update detection.py

--- userbot/modules/detection.py
+++ userbot/modules/detection.py
@@ -1,9 +1,9 @@
-from userbot.utils import edit_or_reply, kyy_cmd
+from userbot.utils import edit_or_reply, joo_cmd
 from userbot import CMD_HELP, bot, CMD_HANDLER as cmd
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 
-@kyy_cmd(pattern="detect(?: |$)(.*)")
+@joo_cmd(pattern="detect(?: |$)(.*)")
 async def detect(event):
     if event.fwd_from:
         return
