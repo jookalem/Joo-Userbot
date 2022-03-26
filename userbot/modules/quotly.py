c46349ff03fb654e780e5756abc1edf2aa69319a
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 16:14:43 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -9,13 +9,13 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_delete, edit_or_reply, kyy_cmd
+from userbot.utils import edit_delete, edit_or_reply, joo_cmd
 from userbot.utils.misc import create_quotly
 
 from .carbon import all_col
 
 
-@kyy_cmd(pattern="q( (.*)|$)")
+@joo_cmd(pattern="q( (.*)|$)")
 async def quotly(event):
     match = event.pattern_match.group(1).strip()
     if not event.is_reply:
