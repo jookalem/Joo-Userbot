238e6f9d2da2742b8f22ad4047b08498e1087f28
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 17:32:54 +0700

    add cmd_hndlr

--- userbot/modules/surat.py
+++ userbot/modules/surat.py
@@ -1,10 +1,10 @@
 from time import sleep
 
 from userbot import CMD_HELP, CMD_HANDLER as cmd
-from userbot.utils import edit_or_reply, kyy_cmd
+from userbot.utils import edit_or_reply, joo_cmd
 
 
-@kyy_cmd(pattern='alfatihah(?: |$)(.*)')
+@joo_cmd(pattern='alfatihah(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
@@ -25,10 +25,44 @@ async def typewriter(typew):
     await xnxx.edit("**ṣirāṭallażīna an'amta 'alaihim gairil-magḍụbi 'alaihim wa laḍ-ḍāllīn**")
     sleep(1)
     await xnxx.edit("**Amin..**")
+
+
+@joo_cmd(pattern='ayatkursi(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await xnxx.edit("**Bismillahir'-rahmanir-rahim**")
+    sleep(1)
+    await xnxx.edit("**Allaahu Laailaaha illa huwal hayyul qayyuum**")
+    sleep(1)
+    await xnxx.edit("**Laa ta'khudzuhu sinatuw walaa nauum**")
+    sleep(1)
+    await xnxx.edit("**Lahuu maa fissamaawaati wamaa fil ardhi**")
+    sleep(1)
+    await xnxx.edit("**Mangdzalladzii yasyfa'u 'indahuu illai bi idznih**")
+    sleep(1)
+    await xnxx.edit("**Ya'lamu maa baina aiydiihim wamaa kholfahum**")
+    sleep(1)
+    await xnxx.edit("*walaa yukhiithuuna bisyayin min 'ilmihii illaa bimaa syaaa**")
+    sleep(1)
+    await xnxx.edit("**wasi'a kursiyyuhus samaawaati wal ardho**")
+    sleep(1)
+    await xnxx.edit("**Walaa yauduhuu khifdhuhumaa wa huwal'aliyyul 'adhiim**")
+    sleep(1)
+    await xnxx.edit("**Alhamdulillah..**")
+
+
 # Create by myself @localheart
+
 
 CMD_HELP.update({
     "surat":
     f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}alfatihah`\
     \n↳ : Surat Alfatihah."
+})
+
+CMD_HELP.update({
+    "surat2":
+    f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}ayatkursi`\
+    \n↳ : Ayat Kursi."
 })
