980d30dca9893c8a230506011f57b89302870026
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-26 10:47:45 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -205,11 +205,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Kyy-Userbot ERROR**\n"
-                    link = "Silahkan chat: @IDnyaKosong"
+                    text = "**Joo-Userbot ERROR**\n"
+                    link = "Silahkan chat: @ikhsanntarjo"
                     text += "Untuk melaporkan kesalahan"
                     text += f"tinggal teruskan pesan ini {link}.\n"
-                    text += "Kyy Siap Membantu Kamu\n"
+                    text += "Joo Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
