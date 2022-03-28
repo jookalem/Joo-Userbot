23905c57f452ab6c5281d05a362f481926085f37
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-28 09:34:32 +0700

    fix typo ngntd

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -183,7 +183,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/jookalem/Jo-Userbot")
+    "https://github.com/jookalem/Joo-Userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Joo-Userbot")
 
