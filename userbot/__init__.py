0a84a0987e697a270c45b7eb22be5abc41dbe4be
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-29 10:57:05 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -824,9 +824,7 @@ with bot:
                         modul_name
                     )
                 )
-                await event.edit(
-                    reply_pop_up_alert, buttons=[Button.inline("Back", data="reopen")]
-                )
+
             else:
                 reply_pop_up_alert = f"Kamu Tidak Diizinkan, Ini Userbot Milik {owner}"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
