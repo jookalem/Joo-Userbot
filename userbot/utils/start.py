930ae314ca90509ed686a8034f1563ed96e730de
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-30 07:05:43 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -12,7 +12,7 @@ async def startupmessage():
             await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/02f87cca391f9b9d627d5.jpg",
-                caption="✨ **Joo-Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @ProjectJoni ",
+                caption="**🥷ᴊσσ-ᴜѕᴇʀвσт🥷 Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @ProjectJoni ",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/JoniSupport"),)],
             )
     except Exception as e:
