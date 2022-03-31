b97c629a1d09e249867dcebc0e10bc7484256dfd
ᴊᴏᴏ <97096980+jookalem@users.noreply.github.com>
2022-03-31 22:26:25 +0700

    Create toxic3.py

---
+++ userbot/modules/toxic3.py
@@ -0,0 +1,150 @@
+from time import sleep
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, joo_cmd
+
+
+@joo_cmd(pattern='bego(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    jo = await edit_or_reply(typew, "**WOYY BOCAH BEGO**")
+    sleep(1)
+    await jo.edit("**JANGAN SO IYE LU DONGO**")
+    sleep(1)
+    await jo.edit("**MUKA MASIH KAYA KONTOL KEBO**")
+    sleep(1)
+    await jo.edit("**GAUSAH BANYAK GAYA LU BEGO**")
+    sleep(1)
+    await jo.edit("**MUKA LU UDAH MIRIP KANEBO**")
+    sleep(1)
+    await jo.edit("**SUKA DI PAKE NGELAP KONTOL KEBO**")
+    sleep(1)
+    await jo.edit("**IDIH NAJIS HINA BAT LU BEGO**")
+    sleep(1)
+    await jo.edit("**MUKA DEKIL ITEM GA KEURUS BEGO**")
+    sleep(1)
+    await jo.edit("**BADAN BAU SAMPAH**")
+    sleep(1)
+    await jo.edit("**JAUH JAUH LU BEGO**")
+
+# Create by myself @ikhsanntarjo
+
+
+@joo_cmd(pattern='tolol(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    jo = await edit_or_reply(typew, "**WOI ANAK TOLOL**")
+    sleep(1)
+    await jo.edit("**YANG SUKA NEKEN TRAMADOL**")
+    sleep(1)
+    await jo.edit("**MUKA LU KAYA KONTOL**")
+    sleep(1)
+    await jo.edit("**GAUSAH SO IYE LU BOCAH TOLOL**")
+    sleep(1)
+    await jo.edit("**MUKA LU MIRIP MONYET BEKANTAN**")
+    sleep(1)
+    await jo.edit("**SERING MAKAN SANTAN**")
+    sleep(1)
+    await jo.edit("**SUKA NGOBROL SAMA SETAN**")
+    sleep(1)
+    await jo.edit("**MUKA LU AJA KAYA ASPAL JALANAN**")
+    sleep(1)
+    await jo.edit("**GAUSAH SO IYE LO BOCAH TOLOL**")
+
+# Create by myself @ikhsanntarjo
+
+
+@joo_cmd(pattern='idiot(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    jo = await edit_or_reply(typew, "**WOI ANAK IDIOT**")
+    sleep(1)
+    await jo.edit("**MUKA LU MIRIP NENEK PEYOT**")
+    sleep(1)
+    await jo.edit("**OTAK LU KECIL KAYA TIKUS GOT**")
+    sleep(1)
+    await jo.edit("**TANGAN BUNTUNG**")
+    sleep(1)
+    await jo.edit("**MUKA KAYA LUTUNG**")
+    sleep(1)
+    await jo.edit("**ITEM DEKIL KAKI BUNTUNG**")
+    sleep(1)
+    await jo.edit("**IDIH NAJIS BOCAH SAGAPUNG**")
+    sleep(1)
+    await jo.edit("**YANG TINGGAL DI LAMPUNG**")
+    sleep(1)
+    await jo.edit("**MUKA LU UDAH MIRIP BOCAH ABK NGENTOT**")
+    sleep(1)
+
+# Create by myself @ikhsanntarjo
+
+
+@joo_cmd(pattern='yatu(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    jo = await edit_or_reply(typew, "`Hai Anak Yatim Piatu`")
+    sleep(1)
+    await jo.edit("`Muka Lu Udah Mirip Si Restu`")
+    sleep(1)
+    await jo.edit("`Item Dekil Idup Lagi`")
+    sleep(1)
+    await jo.edit("`Idih Bocah Yatim Piatu`")
+    sleep(1)
+    await jo.edit("`Kalo Mau Jajan Harus Mulung`")
+    sleep(1)
+    await jo.edit("`Gausah Banyak Gaya Bego Yatim Piatu`")
+    sleep(1)
+    await jo.edit("`Sosoan Jadi Fakboy`")
+    sleep(1)
+    await jo.edit("`Muka Lu Udah Mirip Boboiboy`")
+    sleep(1)
+    await jo.edit("`Najis Jajan Aja Pake Duit Santunan`")
+    sleep(1)
+    await jo.edit("`Gausah Belagu Jadi Fakboy Bego`")
+
+# Create by myself @ikhsanntarjo
+
+
+@joo_cmd(pattern="njs(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    jo = await edit_or_reply(typew, "**Idih Najis**")
+    sleep(1.5)
+    await jo.edit("**Item Dekil Mirip Air Liur Anjing**")
+    sleep(1.5)
+    await jo.edit("**Kalo Keluar Pake Make Up**")
+    sleep(1.5)
+    await jo.edit("**Padahal Muka Lu Item Bat**")
+    sleep(1.5)
+    await jo.edit("**Apalagi Kalo Maghrib**")
+    sleep(1.5)
+    await jo.edit("**Duh Ga Keliatan Anjing**")
+    sleep(1.5)
+    await jo.edit("**Makannya Beli Scincare Bego**")
+    sleep(1.5)
+    await jo.edit("**Biar Muka Lu Ga Di Hina Hina**")
+    sleep(1.5)
+    await jo.edit("**Udah Orang Tua Gaada**")
+    sleep(1.5)
+    await jo.edit("**Malah Minta Di Hina Hina**")
+    sleep(1.5)
+    await jo.edit("**Dasar Bocah Cacat Fisik**")
+    sleep(1.5)
+
+
+CMD_HELP.update({
+    "toxic2": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}bego`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}tolol`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}idiot`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}yatu`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}njs`\
+    \n↳ : Cobain aja."
+})
